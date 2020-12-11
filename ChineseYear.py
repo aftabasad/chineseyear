829a96a36316c1da2b5c18dfed8b728ff78d46fa
aftabasad <aftab.asad@icloud.com>
2020-12-10 22:52:31 -0600

    update

--- ChineseYear.py
+++ ChineseYear.py
@@ -8,7 +8,7 @@ class LaunchRequestHandler(AbstractRequestHandler):
         return is_request_type("LaunchRequest")(handler_input)
 
     def handle(self, handler_input):
-        handler_input.response_builder.speak("Welcome to my Chinese animal skill").set_should_end_session(False)
+        handler_input.response_builder.speak("Welcome to my Chinese year skill").set_should_end_session(False)
         return handler_input.response_builder.response    
 
 class CatchAllExceptionHandler(AbstractExceptionHandler):
@@ -26,15 +26,14 @@ class ChineseYearIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         year = handler_input.request_envelope.request.intent.slots['year'].value
-        speech_text = "Chinese Year Intent triggered"
-       // speech_text = "Your animal is a " + data['Item']['Animal']['S'] + '. Wanna know something else? Apparently you are ' + data['Item']['PersonalityTraits']['S']
+        speech_text = "Your animal is lion"
         handler_input.response_builder.speak(speech_text).set_should_end_session(False)
         return handler_input.response_builder.response    
 
 sb = SkillBuilder()
 sb.add_request_handler(LaunchRequestHandler())
 sb.add_exception_handler(CatchAllExceptionHandler())
-sb.add_request_handler(ChineseAnimalIntentHandler())
+sb.add_request_handler(ChineseYearIntentHandler())
 
 def handler(event, context):
     return sb.lambda_handler()(event, context)
